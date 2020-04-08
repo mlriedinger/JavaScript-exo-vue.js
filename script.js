85d35f321cc3c3de8f24ed9a1468127861212b66
Marie-Laure <marie-laure.riedinger@le-campus-numerique.fr>
2020-04-08 21:11:19 +0200

    Modif fonction updateHeadline

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 var app = new Vue({
-    el: '#app',
-    data: {
-      storeName: 'la boutique de gâteaux',
-      headline: 'Le grand retour du come-back',
-      newHeadline:'',
-      changeColor: false,
-      showImage: false,
+  el: "#app",
+  data: {
+    storeName: "la boutique de gâteaux",
+    headline: "Le grand retour du come-back",
+    newHeadline: "",
+    changeColor: false,
+    showImage: false,
+  },
+  methods: {
+    updateHeadline: function () {
+      this.headline = this.newHeadline;
     },
-    methods: {
-        updateHeadline: function(){
-            this.headline = document.querySelector('#headline').value;
-        }
-    }
-  })
+  },
+});
